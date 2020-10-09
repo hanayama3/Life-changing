3a66a4ba685dc714c8d2214bcbb8ec98725d5478
Ubuntu <ubuntu@ip-172-31-40-229.ap-northeast-1.compute.internal>
2020-10-09 14:38:35 +0000

    flashのjqueryがうまくいかなかったとこから

---
+++ app/assets/javascript/application.js
@@ -0,0 +1,7 @@
+//= require jquery3
+//= require popper
+//= require bootstrap-sprockets
+
+$(function() {
+  setTimeout("$('.time-limit').fadeOut('slow')", 2000) 
+})
--- config/routes.rb
+++ config/routes.rb
@@ -6,8 +6,8 @@ Rails.application.routes.draw do
   }
   
   devise_scope :user do
-  get "sign_in", :to => "users/sessions#new"
-  get "sign_out", :to => "users/sessions#destroy"
+  get "users/sign_in", :to => "users/sessions#new"
+  get "users/sign_out", :to => "users/sessions#destroy"
 end
 
  root to: "home#index"
