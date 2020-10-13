fedb3bf75f33988ebd005630e5b36a80d170bf14
Ubuntu <ubuntu@ip-172-31-40-229.ap-northeast-1.compute.internal>
2020-10-13 12:22:35 +0000

    show作成中

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,13 @@
 class ApplicationController < ActionController::Base
 add_flash_types :success, :warning, :danger
+
+before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  def configure_permitted_parameters
+    added_attrs = [ :name, :email, :password, :password_confirmation　]
+    devise_parameter_sanitizer.permit :sign_up, keys: added_attrs
+    devise_parameter_sanitizer.permit :account_update, keys: added_attrs
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   
+  get 'users/show'
   devise_for :users, :controllers => {
   :registrations => 'users/registrations',
   :sessions => 'users/sessions'
@@ -8,7 +9,8 @@ Rails.application.routes.draw do
   devise_scope :user do
   get "users/sign_in", :to => "users/sessions#new"
   get "users/sign_out", :to => "users/sessions#destroy"
+ 
 end
-
+# get 'users/:id', :to => 'users#show'
  root to: "home#index"
 end
