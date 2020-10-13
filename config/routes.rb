7309a773ddd4ce8f4e1787dc24ee5cc59b05039a
Ubuntu <ubuntu@ip-172-31-40-229.ap-northeast-1.compute.internal>
2020-10-13 14:27:37 +0000

    not_loginのviewから

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,18 @@
 Rails.application.routes.draw do
   
-  get 'users/show'
+  root to: "home#index"
+  
   devise_for :users, :controllers => {
   :registrations => 'users/registrations',
   :sessions => 'users/sessions'
   }
   
   devise_scope :user do
-  get "users/sign_in", :to => "users/sessions#new"
-  get "users/sign_out", :to => "users/sessions#destroy"
- 
+  get "login", to: "users/sessions#new"
+  post "login", to: "users/sessions#create"
+  get "sign_out", to: "users/sessions#destroy"
 end
-# get 'users/:id', :to => 'users#show'
- root to: "home#index"
+
+resources :users, :only => [:show]
+ 
 end
