fd3660eef68789b9211600afe2813b96423bc9dd
Ubuntu <ubuntu@ip-172-31-33-120.ap-northeast-1.compute.internal>
2020-10-20 12:44:03 +0000

    migrationファイル作り直し

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_14_053238) do
+ActiveRecord::Schema.define(version: 2020_10_20_124042) do
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
