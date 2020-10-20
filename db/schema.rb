bb082ae9d850bf746a7115852e006667864e9aa2
Ubuntu <ubuntu@ip-172-31-33-120.ap-northeast-1.compute.internal>
2020-10-20 13:04:56 +0000

    level一旦削除

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_20_124042) do
+ActiveRecord::Schema.define(version: 2020_10_20_123850) do
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -23,7 +23,6 @@ ActiveRecord::Schema.define(version: 2020_10_20_124042) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "image_id"
-    t.integer "level"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
