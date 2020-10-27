119bcc7ee8716c4ae5110e144304553139f2a8ea
Ubuntu <ubuntu@ip-172-31-33-120.ap-northeast-1.compute.internal>
2020-10-27 09:27:56 +0000

    まさかのend漏れ

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_22_012256) do
+ActiveRecord::Schema.define(version: 2020_10_27_085251) do
 
   create_table "habits", force: :cascade do |t|
     t.string "task"
@@ -18,6 +18,16 @@ ActiveRecord::Schema.define(version: 2020_10_22_012256) do
     t.integer "user_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "relationships", force: :cascade do |t|
+    t.integer "follower_id"
+    t.integer "followed_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["followed_id"], name: "index_relationships_on_followed_id"
+    t.index ["follower_id", "followed_id"], name: "index_relationships_on_follower_id_and_followed_id", unique: true
+    t.index ["follower_id"], name: "index_relationships_on_follower_id"
   end
 
   create_table "users", force: :cascade do |t|
--- db/seeds.rb
+++ db/seeds.rb
@@ -3,7 +3,7 @@ name:  "hanayama",
 email: "hanayamabmw@icloud.com",
 password: "hanayama",
 profile: "Rails余裕",
-level: 300
+level: 777
 )
 
 10.times do |n|
@@ -19,8 +19,23 @@ level: level
 )
 end
 
+10.times do |n|
+task = ["勉強", "筋トレ", "瞑想", "断食", "ヨガ", "散歩"]
+frequency = rand(1..7)
+user_id = n+1
 Habit.create!(
-task: "ランニング",
-frequency: 3,
-user_id: 1
+task: task.sample,
+frequency: frequency,
+user_id: user_id
 )
+end
+
+users = User.all
+user  = users.first
+# user_idが2~8までを格納
+following = users[2..8]
+# user_idが3から6までを格納
+followers = users[3..6]
+
+following.each { |followed| user.follow(followed) }
+followers.each { |follower| follower.follow(user) }
