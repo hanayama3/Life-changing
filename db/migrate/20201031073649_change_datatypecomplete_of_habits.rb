91e5eb03416b26422917177846b37ff4015cd7d7
hanayama3 <hanayama.kaoru323@gmail.com>
2020-11-13 22:53:09 +0000

    change_column修正

--- db/migrate/20201031073649_change_datatypecomplete_of_habits.rb
+++ db/migrate/20201031073649_change_datatypecomplete_of_habits.rb
@@ -1,5 +1,9 @@
 class ChangeDatatypecompleteOfHabits < ActiveRecord::Migration[6.0]
   def up
-    change_column :habits, :complete, :integer
+    change_column :habits, :complete, :integer, using: 'complete :: integer'
+  end
+  
+  def down
+    change_column :habits, :complete, :boolean, default: false, null: false
   end
 end
