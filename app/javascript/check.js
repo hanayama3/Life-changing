54e71aace3528d5be67f9228008e9c62a28e87ef
hanayama3 <hanayama.kaoru323@gmail.com>
2020-11-19 14:46:13 +0000

    check.js作成

--- app/javascript/check.js
+++ app/javascript/check.js
@@ -1,38 +1,7 @@
-// $(function() {
-//     setTimeout("$('.complete-content').fadeOut('slow')", 1000) 
-// });
-// 消えた！！
-
-// $(function(){
-//   $('.complete-content').click(function(){
-//     $('#check').slideUp();
-//   });
-// });
-
-// 無理か
-
-// $(function() {
- 
-//   $('input[name="user[habit_ids][]"]').change(function() {
- 
-//     var habit_ids = (this).text("達成");
- 
-//     $('input[name="user[habit_ids][]"]:checked').each(function() {
- 
-//       habit_ids.push($(this).val());
- 
-//     });
- 
-//     $('#p01').text('達成！');
- 
-//   });
-// });
-// １つの要素だけチェックしたら達成！が現れる
-// 全てにチェックって欲しいな
-
-// $(function() {
- 
-//   $('input[name="user[habit_ids][]"]').change(function() {
-// 　$('#check').text('あらあら');
-//   });
-// });
+$(document).on('turbolinks:load',function() {
+    $('input[name="user[habit_ids][]"]').one('click',function() {
+    var i = $('<i>').addClass('fa fa-thumbs-o-up')
+    $(this).before(i);
+    $(this).hide();
+});
+});
